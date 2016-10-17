315851a4260f2364cdfd38e2b9966b196ac8691c
Joshua Achorn <wight@cock.li>
2016-10-16 21:24:31 -0400

    add ayyghost's spam script

---
+++ spamtest.sh
@@ -0,0 +1,8 @@
+# courtesy of @ayyghost
+#!/bin/bash
+while true
+do
+	mx=320;my=256;head -c "$((3*mx*my))" /dev/urandom | convert -depth 8 -size "${mx}x${my}" RGB:- random.png
+	curl -i -X POST -F "file=@random.png" http://localhost:8000/upload
+done
+
