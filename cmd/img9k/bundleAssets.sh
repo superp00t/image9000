a5dfaa8397db9461e38b838da3121d5583d1f082
Joshua Achorn <wight@cock.li>
2019-01-10 02:29:18 -0500

    bundle assets

--- cmd/img9k/bundleAssets.sh
+++ cmd/img9k/bundleAssets.sh
@@ -1,2 +1,5 @@
 go-bindata-assetfs assets/...
-go install -v github.com/superp00t/image9000/cmd/img9k
+# go install -v github.com/superp00t/image9000/cmd/img9k
+git add .
+git commit -m "bundle assets"
+git push
